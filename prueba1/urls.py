d6cef4774c9db8f14682aae3a99cac6bf1427bc9
AnaBarra <gamakaren@hotmail.com>
2022-11-22 20:35:02 -0600

    SegundoParcial

--- prueba1/urls.py
+++ prueba1/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import re_path
 from  . import views
 
 urlpatterns = [
-    path('prueba1/', views.muestra_datos, name= 'prueba1'),
+    #re_path(r'^$', views.muestra_datos, name= 'prueba1'),
+    re_path(r'^$', views.inicio, name= 'inicio'),
+    re_path(r'^registros/', views.muestra_datos, name= 'registros'),
+    re_path(r'^knn/', views.algknn, name= 'knn'),
+    re_path(r'^ingenuo/', views.ingenuo, name= 'ingenuo'),
+    re_path(r'^logistica/', views.logistica, name= 'logistica'),
+    re_path(r'^regresion/', views.regresion, name = 'regresion'),
     
 ]
